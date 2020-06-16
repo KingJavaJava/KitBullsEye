b7a416296dde5882aa0a0387b65af7bac0999d71
hareiwaja <hareiwaja@yahoo.com>
2020-06-16 00:43:45 -0500

    Complete App

---
+++ KitBullsEye/AboutViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AboutViewController.swift
+//  KitBullsEye
+//
+//  Created by Harei Waja on 6/15/20.
+//  Copyright © 2020 Harei Waja. All rights reserved.
+//
+import WebKit
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
